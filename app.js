68c98c1b53d5901890c7878121ce1057e06dc2db
ikesh pack <packappsforyou@gmail.com>
2019-08-11 12:16:00 -0400

    Created server

---
+++ app.js
@@ -0,0 +1,12 @@
+// This creates a new Express server.
+const express = require("express");
+const app = express();
+
+// Tells the server which potrt to run on.
+const port = process.env.PORT || 5000;
+
+// Sets up a basic route so that we can render some information on our page.
+app.get("/", (req, res) => res.send("Hello World"));
+
+// Tells Express to start a socket and listen for connections on the path.
+app.listen(port, () => console.log(`Server is running on port ${port}`));
