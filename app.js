648b74ae26c5e5225fcb1b6fe39ca7263e0e994b
ikesh pack <packappsforyou@gmail.com>
2019-08-11 12:20:04 -0400

    Added script to allow nodemon to watchfiles and hot reload server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Sets up a basic route so that we can render some information on our page.
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Sistine Camels"));
 
 // Tells Express to start a socket and listen for connections on the path.
 app.listen(port, () => console.log(`Server is running on port ${port}`));
