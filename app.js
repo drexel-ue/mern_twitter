304c59cb4961095e4bd8eb0f95c9651f3003fc64
ikesh pack <packappsforyou@gmail.com>
2019-08-11 14:19:40 -0400

    Added route for registering user

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
-
 // This creates a new Express server.
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
-
 // Imports created routes.
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-
 // Allows us to parse the json sent to the front end.
 const bodyParser = require("body-parser");
 
@@ -24,13 +21,14 @@ const port = process.env.PORT || 5000;
 // Sets up a basic route so that we can render some information on our page.
 app.get("/", (req, res) => res.send("Sistine Camels"));
 
+// Parse application/x-www-form-urlencoded.
+app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application/json.
+app.use(bodyParser.json());
+
 // We must tell Express to use imported routes.
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-
-// Adds middleware for body parser.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Tells Express to start a socket and listen for connections on the path.
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,49 @@
+// Needed to create the router.
 const express = require("express");
+// Handles directing requests to the desired handlers.
 const router = express.Router();
+// Used to encrypt passwords.
+const bcrypt = require("bcryptjs");
+// User model.
+const User = require("../../models/User");
 
 // NB: The callback for every Express route requires a request and response as arguments.
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+// Handles creation of new users.
+router.post("/register", (req, res) => {
+  const body = req.body;
+  // First check to make sure this email is not already in use.
+  User.findOne({ email: body.email }).then(user => {
+    if (user) {
+      // Throw a 400 user if the email is already in use.
+      return res.status(400).json({ email: "This email is already in use." });
+    } else {
+      // Else, create a new user.
+      const newUser = new User({
+        handle: body.handle,
+        email: body.email,
+        password: body.password
+      });
+
+      // Salt the password.
+      bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+          // Throw an error if there is one.
+          if (error) throw error;
+          // Set the newUser object's password to the salted password (hash).
+          newUser.password = hash;
+          newUser
+            // Save the new user document to the database.
+            .save()
+            // Return a json object representing the new user document as processed by the databse.
+            .then(user => res.json(user))
+            // Log any errors.
+            .catch(error => console.log(error));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
