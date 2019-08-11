4f61ab37ae3ec198a22acc81734c9ac0ac89d3d7
ikesh pack <packappsforyou@gmail.com>
2019-08-11 16:24:49 -0400

    Added protected route for posting tweets

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -34,4 +34,24 @@ router.get("/:id", (req, res) => {
     );
 });
 
+// Protected route for posting tweets.
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateTweetInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newTweet = new Tweet({
+      text: req.body.text,
+      user: req.user.id
+    });
+
+    newTweet.save().then(tweet => res.json(tweet));
+  }
+);
+
 module.exports = router;
