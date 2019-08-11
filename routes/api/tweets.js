b545a3dc1081cc7d55aabef035c2bd5172817d23
ikesh pack <packappsforyou@gmail.com>
2019-08-11 16:23:15 -0400

    Added routes for serving tweets

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Tweet = require("../../models/Tweet");
+const validateTweetInput = require("../../validation/tweets");
 
 // NB: The callback for every Express route requires a request and response as arguments.
-router.get("/test", (req, res) =>
-  res.json({ msg: "This is the tweets route" })
-);
+
+// Serves all tweets ordered by date.
+router.get("/", (req, res) => {
+  Tweet.find()
+    .sort({ date: -1 })
+    .then(tweets => res.json(tweets))
+    .catch(err => res.status(404).json({ notweetsfound: "No tweets found" }));
+});
+
+// Serves all tweets for a specified user.
+router.get("/user/:user_id", (req, res) => {
+  Tweet.find({ user: req.params.user_id })
+    .then(tweets => res.json(tweets))
+    .catch(err =>
+      res.status(404).json({ notweetsfound: "No tweets found from that user" })
+    );
+});
+
+// Serves a specified tweet.
+router.get("/:id", (req, res) => {
+  Tweet.findById(req.params.id)
+    .then(tweet => res.json(tweet))
+    .catch(err =>
+      res.status(404).json({ notweetfound: "No tweet found with that ID" })
+    );
+});
 
 module.exports = router;
