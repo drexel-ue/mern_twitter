b1a8f92cb2ec1efe0eb10c760a958b20cbe164de
ikesh pack <packappsforyou@gmail.com>
2019-08-11 20:00:53 -0400

    Added protected routed for tweets

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -14,6 +14,14 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+      <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
+      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
+      <ProtectedRoute
+        exact
+        path="/new_tweet"
+        component={TweetComposeContainer}
+      />
     </Switch>
   </div>
 );
